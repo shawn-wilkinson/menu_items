051e044f1998c639870438c8cd635b876022d875
Shawn Wilkinson <shawnwilkinson11@gmail.com>
2015-11-09 16:09:34 -0600

    refactor slightly and add readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# menu_items
+######Solution to Table XI Coding Challenge
+######Shawn Wilkinson
+
+Thanks for taking the time to look at my solution. If you have any quesitons, please feel free to reach out.
+
+######Usage Instructions:
+
+The program can be run from the command line using ARGV to specify which file contains the list you would like to analyze. Simply enter 'ruby menu.rb' followed by the name of the file. For example, if you would like to run the included 'menu.txt' file, you would type ```ruby menu.rb menu.txt```
--- menu.rb
+++ menu.rb
@@ -7,25 +7,20 @@ class Item
 end
 
 class List
-
     def initialize(file_name)
-        @total_price = nil
+        @target_price = nil
         @file = File.open(file_name)
         @items = []
         @item_count = Hash.new
         setup_list
-        if find_mix(@items,@total_price)
-            output_result
-        else
-            output_no_result_found
-        end
+        process_list
     end
 
     def setup_list
         @file.each do |line|
             lines = line.chomp.split(',')
-            if @total_price == nil
-                @total_price = lines.first.delete('$').to_f
+            if @target_price == nil
+                @target_price = lines.first.delete('$').to_f
             else
                 @item_count[lines[0]] = 0
                 @items << Item.new(lines[0],lines[1].delete('$'))
@@ -34,18 +29,27 @@ class List
         @items = @items.sort_by{|item| item.price}.reverse
     end
 
-    def find_mix(item_array, remainder)
-        current_item= item_array.first
-        return false if @items.last.price > @total_price
+    def process_list
+        if item_prices_too_high?
+            output_no_result_found
+        elsif find_combination(@items,@target_price)
+            output_result
+        else
+            output_no_result_found
+        end
+    end
+
+    def find_combination(item_array, remainder)
+        current_item = item_array.first
         if evenly_divisible?(remainder,current_item.price)
-             @item_count[current_item.name] += (remainder / current_item.price).to_i
+             @item_count[current_item.name] = (remainder / current_item.price).to_i
             return true
         end
         return false if item_array.length == 1
         n = (remainder / current_item.price).to_i
         while n >= 0
             new_remainder = remainder - (n * current_item.price)
-            if find_mix(item_array[1..-1],new_remainder)
+            if find_combination(item_array[1..-1],new_remainder)
                 @item_count[current_item.name] = n
                 return true
             end
@@ -55,28 +59,27 @@ class List
     end
 
     def output_result
-        puts "To spend a total of $#{sprintf('%.2f', @total_price)}, buy the following:"
+        puts "To spend a total of $#{sprintf('%.2f', @target_price)}, buy the following:"
         @item_count.each do |name, qty|
             puts "#{qty} #{name} at $#{sprintf('%.2f', price_lookup(name))} each" if qty > 0
         end
     end
 
     def output_no_result_found
-        puts "There is no possible combination to reach a total of $#{@total_price}"
+        puts "There is no possible combination to reach a total of $#{@target_price}"
+    end
+
+    def item_prices_too_high?
+        @items.all?{|item| item.price > @target_price}
     end
 
     def evenly_divisible?(large,small)
-        # (((large.to_f / small.to_f) * 100).round / 100.00) % 1 == 0
         (large.to_f / small.to_f).round(4) % 1 == 0
     end
 
     def price_lookup(input_name)
         @items.find{|item| item.name == input_name}.price
     end
-
-
-
-
 end
 
-new_list = List.new('menu.txt')
+new_list = List.new(ARGV[0])
